95ebd9cc807f8d1f3c7af991e747db3510fc1869
Aidan Crispin <acrispin5466@gmial.com>
2019-10-04 09:54:09 -0500

    Classwork

--- src/youDoIt/distanceFromAverage.java
+++ src/youDoIt/distanceFromAverage.java
@@ -1,30 +1,35 @@
 package youDoIt;
 import java.util.Scanner;
 public class distanceFromAverage {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        boolean quit = false;
-        double userInput;
-        double differnce;
+        final int LIMIT = 20;
+        double userInput = 0;
+        double difference;
         double average;
-        double sum =0;
-        double [] userArray = new double [20];
-            for(int i = 0; i < userArray.length; i ++) {
+        double sum = 0;
+        double[] userArray = new double[20];
+        if (userInput != 999) {
+            for (int i = 0; i < LIMIT; i++) {
                 System.out.println("Enter quiz score number " + (i + 1) + ">>>> ");
                 userInput = input.nextDouble();
-                if (userInput == 999) {
-                    quit = true;
-                }
                 sum = userInput + sum;
-                average = sum / userArray.length;
-                userArray[i] = userInput;
-                    }
-        if(quit == true){
-            for(int j = 0; j < userArray.length; j ++) {
-                average = sum / userArray.length;
-                differnce = userArray[j]-average;
-                System.out.println(userArray[j] + " is " + differnce + " away from the average.");
+                if (userInput < 999) {
+                    userArray[i] = userInput;
+                }
+                if (userInput == 999 && userArray[0] == 0) {
+                    userArray = new double[i];
+                    i = LIMIT;
+                    System.out.println("ERROR no numbers entered.");
+                } else if (userInput == 999) {
+                    userArray = new double[i];
+                    i = LIMIT;
+                    average = sum / userArray.length;
+                    difference = userArray[i] - average;
+                    System.out.println(userArray[i] + " is " + difference + " away from the average.");
+                    System.out.println("The average is " + average);
                 }
             }
+        }
     }
 }
