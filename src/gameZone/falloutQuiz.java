ca2eaa6317e127fdde1dc35e0fab186d9019365a
Aidan Crispin <acrispin5466@gmial.com>
2019-10-10 08:39:22 -0500

    classwork

--- src/gameZone/falloutQuiz.java
+++ src/gameZone/falloutQuiz.java
@@ -7,12 +7,20 @@ public class falloutQuiz {
         boolean quit = true;
         String converter;
         int count = 0;
-        char dataValidation [] = {};
+        String questions [] = {
+                "When was the first Fallout game released?",
+                "Which virus caused humans to mutate into super mutants?",
+                "Where is Fallout 3 set in?",
+                ""
+        };
+        char dataValidation [] = {'a', 'A', 'b', 'B', 'c', 'C'};
         char correctAnswers [] = {'a','b','c','b','a','b','c','b','a','b'};
         char userEntry;
         int correct = 0;
         int incorrect = 0;
         while (quit) {
+            for (int i = 0; i < count; i++) {
+                System.out.println(questions[i]);
             System.out.println("Please choose a, b, or c.\n>>>>>");
             converter = input.next();
             userEntry = converter.charAt(0);
@@ -20,8 +28,7 @@ public class falloutQuiz {
                 quit = false;
             }
             count++;
-        }
-        for (int i = 0; i < count; i++) {
+            }
         }
     }
 }
